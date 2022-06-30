4e665979e52791f7be9da0c1208e3888be5e5b62
lei.tian <whereismoney@qq.com>
2022-06-30 19:05:11 +0800

    fix TestCase.php

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -15,33 +15,11 @@
 
 namespace ip2region\Tests;
 
-use ip2region\XdbSearcher;
-
 class TestCase extends \PHPUnit\Framework\TestCase
 {
     public static function now()
     {
         return microtime(true) * 1000;
-    }
-
-    public function test()
-    {
-        $ip = '1.2.3.4';
-        $xdb = './ip2region.xdb';
-        try {
-            // 1、加载整个 xdb 到内存。
-            $cBuff = XdbSearcher::loadContentFromFile($xdb);
-            if (null === $cBuff) {
-                throw new \RuntimeException("failed to load content buffer from '$xdb'");
-            }
-            // 2、使用全局的 cBuff 创建带完全基于内存的查询对象。
-            $searcher = XdbSearcher::newWithBuffer($cBuff);
-            // 3、查询
-            $region = $searcher->search($ip);
-            var_dump($region);
-        } catch (\Exception $e) {
-            var_dump($e->getMessage());
-        }
     }
 
 }
